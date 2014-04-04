729e554d12ebdb0ebd5489cb40776208dba6988d
Sean <sean.bartolay@gmail.com>
2014-04-04 15:42:39 +0800

    Initial version

---
+++ test/js/src/controllers/CorrectController.js
@@ -0,0 +1,7 @@
+angular.module ("app").controller ('CorrectController',["$scope", "$log", function($scope, $log){
+    $scope.data = ["data1", "data2"];
+    
+    $scope.onItemClicked = function () {
+        // do something here
+    };
+}]);
